b05b1c35d8d5a4345a208ca8bb17c7d34422b511
krishnagiriai-source <krishnagiri.ai@gmail.com>
2026-03-07 12:43:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  BIN_ID:     '69aaa6b4d0ea881f40f4bc9e',
+  API_KEY:    '$2a$10$uWqcCac6wqbP1FGPzHIbteVQw0ih0gN9BQx.bGoirjdfdaKSVjCl6',
+  ADMIN_PASS: 'MRK@2026',
+  COMPANY:    'MRK Foods Private Limited',
+  ADDRESS:    'Vijay House, Bhandup West, Mumbai',
+  PHONE:      '+91 81087 16260',
+  PHONE_TEL:  '+918108716260',
+};
